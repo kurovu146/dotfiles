7d860b1f974b92358931193881516b78be8676ff
kurovu146 <kurobakaito146@gmail.com>
2025-12-16 15:31:22 +0700

    update theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -5,25 +5,47 @@ return {
     priority = 1001,
     opts = {
       style = "night",
-      transparent = true,
+
+      -- ❌ TẮT hoàn toàn transparent
+      transparent = false,
+
       styles = {
         comments = { italic = true },
         keywords = { italic = true },
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-      on_highlights = function(hl, c)
-        -- 🎨 Làm vạch chia sáng rõ hơn
-        hl.VertSplit = { fg = c.border_highlight or c.blue, bg = "NONE" }
-        hl.WinSeparator = { fg = c.blue, bg = "NONE" }
 
-        -- 🪵 Làm border của Neo-tree rõ hơn
-        hl.NeoTreeWinSeparator = { fg = c.blue, bg = "NONE" }
+        -- Sidebar & float dùng nền chuẩn
+        sidebars = "dark",
+        floats = "dark",
+      },
+
+      -- 🎨 Tinh chỉnh highlight để giống ảnh
+      on_highlights = function(hl, c)
+        -- Editor
+        hl.Normal = { fg = c.fg, bg = c.bg }
+        hl.NormalNC = { fg = c.fg, bg = c.bg }
+        hl.NormalFloat = { fg = c.fg, bg = c.bg_dark }
+
+        -- Sidebar (Neo-tree, etc.)
+        hl.NeoTreeNormal = { fg = c.fg_sidebar, bg = c.bg_sidebar }
+        hl.NeoTreeEndOfBuffer = { fg = c.bg_sidebar, bg = c.bg_sidebar }
+
+        -- Separator giống VSCode / ảnh
+        hl.WinSeparator = { fg = c.border, bg = c.bg }
+        hl.VertSplit = { fg = c.border, bg = c.bg }
+
+        -- Tab / Buffer
+        hl.TabLine = { fg = c.comment, bg = c.bg_dark }
+        hl.TabLineSel = { fg = c.fg, bg = c.bg }
+        hl.TabLineFill = { bg = c.bg_dark }
+
+        -- Terminal
+        hl.Terminal = { fg = c.fg, bg = c.bg_dark }
       end,
     },
+
     config = function(_, opts)
       require("tokyonight").setup(opts)
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd.colorscheme("tokyonight")
     end,
   },
 }
